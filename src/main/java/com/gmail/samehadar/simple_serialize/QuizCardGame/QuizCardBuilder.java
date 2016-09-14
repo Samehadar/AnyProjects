38b54e376306957ad5161b3ce968bfbd052582d2
samehadar <vlyutarevich@hwdtech.com>
2016-09-14 22:51:01 +0600

    Add QuizCardBuilder class

---
+++ src/main/java/com/gmail/samehadar/simple_serialize/QuizCardGame/QuizCardBuilder.java
@@ -0,0 +1,130 @@
+package com.gmail.samehadar.simple_serialize.QuizCardGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Create card
+ */
+public class QuizCardBuilder {
+
+    private JTextArea questionArea;
+    private JTextArea answerArea;
+    private ArrayList<QuizCard> cardList;
+    private JFrame frame;
+
+    public static void main(String[] args) {
+        QuizCardBuilder builder = new QuizCardBuilder();
+        builder.go();
+    }
+
+    public void go() {
+        frame = new JFrame("Quiz Card Builder");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+        questionArea = new JTextArea(6, 20);
+        questionArea.setLineWrap(true);
+        questionArea.setWrapStyleWord(true);
+        questionArea.setFont(bigFont);
+
+        JScrollPane questionScroller = new JScrollPane(questionArea);
+        questionScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        questionScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        answerArea = new JTextArea(6, 20);
+        answerArea.setLineWrap(true);
+        answerArea.setWrapStyleWord(true);
+        answerArea.setFont(bigFont);
+
+        JScrollPane answerScroller = new JScrollPane(answerArea);
+        answerScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        answerScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        JButton nextButton = new JButton("Next Card");
+
+        cardList = new ArrayList<QuizCard>();
+
+        JLabel questionLabel = new JLabel("Question: ");
+        JLabel answerLabel = new JLabel("Answer: ");
+
+        mainPanel.add(questionLabel);
+        mainPanel.add(questionScroller);
+        mainPanel.add(answerLabel);
+        mainPanel.add(answerScroller);
+        mainPanel.add(nextButton);
+        nextButton.addActionListener(new NextCardListener());
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        newMenuItem.addActionListener(new NewMenuListener());
+
+        saveMenuItem.addActionListener(new SaveMenuListener());
+        fileMenu.add(newMenuItem);
+        fileMenu.add(saveMenuItem);
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+    }
+
+    public class NextCardListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(questionArea.getText(), answerArea.getText());
+            cardList.add(card);
+            clearCard();
+        }
+    }
+
+    public class SaveMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            QuizCard card = new QuizCard(questionArea.getText(), answerArea.getText());
+            cardList.add(card);
+
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.showSaveDialog(frame);
+            saveFile(fileChooser.getSelectedFile());
+        }
+    }
+
+    public class NewMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            cardList.clear();
+            clearCard();
+        }
+    }
+
+    private void ClearCard() {
+        questionArea.setText("");
+        answerArea.setText("");
+        questionArea.requestFocus();
+    }
+
+    private void saveFile(File file) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            for (QuizCard card: cardList) {
+                writer.write(card.getQustion() + '/');
+                writer.write(card.getAnswer() + '\n');
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+    }
+
+}
